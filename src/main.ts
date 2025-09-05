0a8ef9cb0367e84732aeb557266e11365e4836f7
Nathy1208 <smaniotonathaly0@gmail.com>
2025-09-05 11:35:03 -0300

    05/09/2025

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+ // Habilita o CORS para que seu front-end possa acessar a API
+  app.enableCors({
+    origin: 'http://localhost:5173', // A URL do seu front-end (Vite usa 5173 por padrão)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
